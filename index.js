109de167dc1411ccf37d0e3a1dc0061c1042dde5
geiver12 <sosdani2000@hotmail.com>
2021-04-23 21:49:40 -0400

    other opturnity

--- index.js
+++ index.js
@@ -3,26 +3,25 @@ const express = require('express');
 const app = express();
 const request = require("request");
 
-const PORT = process.env.PORT || 8000
+const PORT = process.envPORT || 8000
+
+app.get('/', function (req, res) {
+    request("https://www.instagram.com/web/search/topsearch/?context=blended&query=instaescuela", function (error, response, body) {
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body);
+        res.json({ message: 'Hola Mundo!', data:body});
+    });
+
+});
+
+app.get('/test', function (req, res) {
+   res.send('esta funciona perfectamente')
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
 
-app.get('/', function (req, res) {
-    request("https://www.instagram.com/web/search/topsearch/?context=blended&query=instaescuela", function (error, response, body) {
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body);
-        res.json({ message: 'Hola Mundo!'});
-    });
-
-});
-
-app.get('/test', function (req, res) {
-   
-   res.send('esta funciona perfectamente')
-
-});
 
 
