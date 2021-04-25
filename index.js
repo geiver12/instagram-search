44a56638d17cf5fef347c30afb1e832077fa901d
geiver12 <sosdani2000@hotmail.com>
2021-04-25 00:03:40 -0400

    probando un buscador

--- index.js
+++ index.js
@@ -2,20 +2,32 @@ const http = require("http");
 const express = require('express');
 const app = express();
 const request = require("request");
+const axios = require("axios");
 
 const PORT = process.envPORT || 8000
 
-app.get('/', function (req, res) {
-    request("https://www.instagram.com/web/search/topsearch/?context=blended&query=instaescuela", function (error, response, body) {
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body);
-        res.json({ message: 'Hola Mundo!', data:body});
-    });
+app.get('/', async function (req, res) {
+  try {
+      //  const query = req.body.query;
+        //console.log(query)
+        await axios.get("https://searchusers.com/search/geiver").then(response => {
+            console.log(response.data)
+            res.send({ data: response.data })
+        })
+
+        //https://www.instagram.com/{username}/?__a=1
+        //https://www.instagram.com/web/search/topsearch/?context=blended&query=${query}
+
+
+
+    } catch (error) {
+        console.error(error)
+    }
 
 });
 
 app.get('/test', function (req, res) {
-   res.send('esta funciona perfectamente')
+    res.send('esta funciona perfectamente')
 });
 
 app.listen(PORT, () => {
